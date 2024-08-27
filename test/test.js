6655236d9217cd0f3f103a6fd58515c92b25c7c6
Chris Burgess <chris@giantrobot.co.nz>
2024-08-27 21:49:01 +1200

    Refactor so we can test parseRSS

--- test/test.js
+++ test/test.js
@@ -1,20 +1,18 @@
 const test = require("ava");
-const { fetchRSS } = require("../toots.js");
+const { parseRSS } = require("../toots.js");
 const fs = require('fs').promises;
 const path = require('path');
 
-test("Test fetchRSS method", async t => {
+test("Test parseRSS method", async t => {
   const fixturePath = path.join(__dirname, 'fixtures', 'sample_feed.xml');
   const sampleFeed = await fs.readFile(fixturePath, 'utf-8');
 
-  const fetchedItems = await fetchRSS('https://example.com/feed.xml');
+  const fetchedItems = await parseRSS(sampleFeed);
 
   t.truthy(fetchedItems, "Items should be fetched");
-  t.is(fetchedItems.length, 2, "Should have 2 items");
-  t.is(fetchedItems[0].title, "Sample Toot 1", "First item should have correct title");
-  t.is(fetchedItems[1].title, "Sample Toot 2", "Second item should have correct title");
-  t.is(fetchedItems[0].link, "https://example.com/toot1", "First item should have correct link");
-  t.is(fetchedItems[1].link, "https://example.com/toot2", "Second item should have correct link");
-  t.is(fetchedItems[0].content, "This is the content of sample toot 1", "First item should have correct content");
-  t.is(fetchedItems[1].content, "This is the content of sample toot 2", "Second item should have correct content");
+  t.is(fetchedItems.length, 12, "Should have 12 items");
+  t.regex(fetchedItems[0].content, /finally taking the dog for a walk/, "First item should have expected content");
+  t.regex(fetchedItems[1].content, /go press &quot;play&quot; on the line charts/, "First item should have expected content");
+  t.is(fetchedItems[0].link, "https://example.org/@exampleuser/113031903914816708", "First item should have correct link");
+  t.is(fetchedItems[1].link, "https://example.org/@exampleuser/113027825347296904", "Second item should have correct link");
 });
--- toots.js
+++ toots.js
@@ -8,22 +8,28 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 const Parser = require('rss-parser');
 
 async function fetchRSS(rssURL) {
-  let rssData = await EleventyFetch(rssURL, {
+  return await EleventyFetch(rssURL, {
     duration: "1d", // Cache the result for one day
     type: "text"    // Parse as text
   });
+}
 
+async function parseRSS(rssData) {
   let parser = new Parser();
   let feed = await parser.parseString(rssData);
-
   return feed.items;
 }
 
 module.exports = function(eleventyConfig, options) {
   eleventyConfig.addGlobalData("toots", async () => {
-    return await fetchRSS(options.url);
+    const rssData = await fetchRSS(options.url);
+    return await parseRSS(rssData);
   });
-  eleventyConfig.addFilter('toot_json', function(obj) {
+
+  eleventyConfig.addFilter('toot_json', (obj) => {
     return JSON.stringify(obj, null, 2);
-  })
+  });
 };
+
+module.exports.fetchRSS = fetchRSS;
+module.exports.parseRSS = parseRSS;
